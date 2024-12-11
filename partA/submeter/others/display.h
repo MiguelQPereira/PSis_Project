4bd5dfd551c0143e8be794dde8e462161af36a15
MiguelQPereira <miguelquinapereira@tecnico.ulisboa.pt>
2024-12-11 16:38:12 +0000

    para testes

---
+++ partA/submeter/others/display.h
@@ -0,0 +1,4 @@
+#include "structs.h"
+
+// Function to draw the display
+int display (player_data_t players[8], alien_data_t aliens[N_ALIENS], pewpew_t zaps[2][16], time_t time, WINDOW * space, WINDOW * score_board);
